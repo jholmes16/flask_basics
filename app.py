edd45a4cf2c8b2d51b6a74a11324b473da96e1cd
jholmes16 <jholmesarea@gmail.com>
2022-03-29 11:44:00 -0400

    creating and getting form data

--- app.py
+++ app.py
@@ -22,5 +22,19 @@ def query():
     location = request.args.get('location')
     return '<h1>Hi {}.  You are from {}.  You are on the query page!</h1>'.format(name, location)
 
+@app.route('/theform')
+def theform():
+    return  '''<form method="POST" action="/process">
+                <input type="text" name="name">
+                <input type="text" name="location">
+                <input type="submit" value="Submit">
+            </form>'''
+
+@app.route('/process', methods=['POST'])
+def process():
+    name = request.form['name']
+    location = request.form['location']
+    return '<h1>Hello {}.  You are from {}.  You have submitted the form successfully!</h1>'.format(name, location)
+
 if __name__ == "__main__":
     app.run(debug=True)
