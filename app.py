2177faf070441be3b254be134516e22b8e712212
jholmes16 <jholmesarea@gmail.com>
2022-03-30 11:13:59 -0400

    use get and post request in same method

--- app.py
+++ app.py
@@ -22,19 +22,19 @@ def query():
     location = request.args.get('location')
     return '<h1>Hi {}.  You are from {}.  You are on the query page!</h1>'.format(name, location)
 
-@app.route('/theform')
+@app.route('/theform', methods=['GET', 'POST'])
 def theform():
-    return  '''<form method="POST" action="/process">
-                <input type="text" name="name">
-                <input type="text" name="location">
-                <input type="submit" value="Submit">
-            </form>'''
 
-@app.route('/process', methods=['POST'])
-def process():
-    name = request.form['name']
-    location = request.form['location']
-    return '<h1>Hello {}.  You are from {}.  You have submitted the form successfully!</h1>'.format(name, location)
+    if request.method == 'GET':
+        return  '''<form method="POST" action="/theform">
+                    <input type="text" name="name">
+                    <input type="text" name="location">
+                    <input type="submit" value="Submit">
+                </form>'''
+    else:
+        name = request.form['name']
+        location = request.form['location']
+        return '<h1>Hello {}.  You are from {}.  You have submitted the form successfully!</h1>'.format(name, location)
 
 @app.route('/processjson', methods=['POST'])
 def processjson():
