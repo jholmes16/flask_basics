43aa610b167dc6acd2b3be5e9be19461bf0e211e
jholmes16 <jholmesarea@gmail.com>
2022-04-08 13:32:55 -0400

    create db, connect db, view db results

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, jsonify,request, url_for, redirect, session, render_template
+from flask import Flask, jsonify,request, url_for, redirect, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
 #configuration values link https://flask.palletsprojects.com/en/2.1.x/config/
 app.config['DEBUG'] = True #also auto reload screen on changes
 app.config['SECRET_KEY'] = 'thisisasecret!' #needed for sessions to store data
+
+def connect_db():
+    sql = sqlite3.connect('C:/Users/JahmaulHolmes/Flask/data.db')
+    sql.row_factory = sqlite3.Row
+    return sql
+
+def get_db():
+    if not hasattr(g, 'sqlite3'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def index():
@@ -53,6 +69,13 @@ def processjson():
     randomlist = data['randomlist']
 
     return jsonify({'result' : 'Success!', 'name' : name, 'location' : location, 'randomkeyinlist' : randomlist[1]})
+
+@app.route('/viewresults')
+def viewresults():
+    db = get_db()
+    cur = db.execute('select id, name, location from users')
+    results = cur.fetchall()
+    return '<h1>The ID is {}.  The name is {}. The location is {}.</h1>'.format(results[0]['id'], results[0]['name'], results[0]['location'])
 
 if __name__ == "__main__":
     app.run()
